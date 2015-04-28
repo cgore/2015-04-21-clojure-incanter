8d44a4a5347c61134190e2d5fa760c801ce4539a
Christopher Mark Gore <cgore@cgore.com>
2015-04-28 14:51:39 -0500

    Finishing the slides.

--- code/src/code/core.clj
+++ code/src/code/core.clj
@@ -1,13 +1,15 @@
 (ns code.core
   "Howdy Incanter!"
   (:require [incanter.core :as i
-             :refer [$ conj-cols conj-rows dataset
-                     dim to-dataset view]]
+             :refer [$ $order $rollup $where conj-cols
+                     conj-rows dataset dim save
+                     to-dataset view]]
+            [incanter.datasets :as ids]
             [incanter.stats :as is]
-            [incanter.charts :as ic
-                             :refer [histogram]]
+            [incanter.charts :as ic :refer [histogram]]
             [incanter.io :as iio
                          :refer [read-dataset]]))
+
 
 ;;; Simple plot of a sine wave.
 (view (ic/function-plot #(Math/sin %) -4 4
@@ -52,6 +54,9 @@
         "/Rdatasets/csv/datasets/AirPassengers.csv")
    :header true))
 
+;;; There are lots of sample data sets included with incanter.
+(def hec (ids/get-dataset :hair-eye-color))
+
 ;;; The $ operator is a shortcut to get that column of data out of a dataset.
 (defn x [dataset]
   ($ :x dataset))
@@ -65,11 +70,41 @@
 (defn mpi [dataset]
   ($ (keyword "Monthly Personal Income") dataset))
 
-(let [mpi-filtered (filter #(< 0 %) (mpi pass-data))
-      mpi-log10 (map #(Math/log10 %) mpi-filtered)]
-  (view (histogram mpi-log10
-                   :x-label "Monthly Personal Income"
-                   :nbins 50)))
+(def mpi-filtered (filter #(< 0 %) (mpi pass-data)))
+
+(let [mpi-log10 (map #(Math/log10 %) mpi-filtered)]
+  (view (ic/histogram mpi-log10
+                      :x-label "Monthly Personal Income"
+                      :nbins 50)))
+
+
+(i/with-data (ids/get-dataset :co2)
+  (view (ic/bar-chart :Type :uptake
+                      :title "CO2 Uptake"
+                      :group-by :Treatment
+                      :x-label "Grass Types" :y-label "Uptake"
+                      :legend true)))
+
+
+(i/with-data (ids/get-dataset :iris)
+  (view (ic/scatter-plot :Sepal.Length :Sepal.Width
+                         :group-by :Species)))
+
+
+(i/with-data ($rollup i/sum :count [:hair :eye] hec)
+  (view (ic/line-chart :hair :count)))
+
+(i/with-data ($rollup :mean :count [:hair :eye] hec)
+  (view (ic/bar-chart :hair :countq
+                      :group-by :eye
+                      :legend true)))
+
+;;; Statistics!
+(def mpi-stats {:mean (is/mean mpi-filtered)
+                :variance (is/variance mpi-filtered)
+                :standard-deviation (is/sd mpi-filtered)
+                :median (is/median mpi-filtered)
+                :kurtosis (is/kurtosis mpi-filtered)})
 
 (defn -main
   [& args])
